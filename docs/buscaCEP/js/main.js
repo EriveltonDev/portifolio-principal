04d75545aeaf4877b20f1579262ccc8f7e6d8a63
EriveltonDev <eriveltondesousa26@gmail.com>
2021-12-18 14:51:21 -0300

    melhorias no buscador de CEP

--- docs/buscaCEP/js/main.js
+++ docs/buscaCEP/js/main.js
@@ -7,20 +7,37 @@ const localidade = document.querySelector('#localidade');
 const UF = document.querySelector('#uf');
 
 botao.addEventListener('click', () => {
+    if (buscador.value.length === 8) {
     puxarCEP(buscador.value)
+    } else {
+        window.alert('CEP inválido');
+        limpar();
+    }
 });
 
+function limpar() {
+    buscador.value = '';
+    logradouro.value = '';
+    complemento.value = '';
+    bairro.value = '';
+    localidade.value = '';
+    UF.value = '';
+}
 
 function puxarCEP(numCEP) {
     const cep = fetch('https://viacep.com.br/ws/' + numCEP + '/json/');
 
     cep.then(r => r.json())
     .then(body => {
-        console.log(body)
-        logradouro.value = body.logradouro;
-        complemento.value = body.complemento;
-        bairro.value = body.bairro;
-        localidade.value = body.localidade;
-        UF.value = body.uf;
+        if (body.logradouro != undefined) {
+            logradouro.value = body.logradouro;
+            complemento.value = body.complemento;
+            bairro.value = body.bairro;
+            localidade.value = body.localidade;
+            UF.value = body.uf;
+        } else {
+            window.alert('Endereço não encontrado ou não existe');
+            limpar();
+        }
     })
 }
